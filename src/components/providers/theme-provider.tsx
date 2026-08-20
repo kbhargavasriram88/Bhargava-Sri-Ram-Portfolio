4c0d7f49a2e63acca9410c55724cc5d78fc3819f
kbhargavasriram88 <k.bhargavasriram88@gmail.com>
2026-08-20 11:25:15 +0530

    Fix React 19 script tag hydration error in ThemeProvider and nativeButton warning in DialogTrigger

--- src/components/providers/theme-provider.tsx
+++ src/components/providers/theme-provider.tsx
@@ -1,21 +1,21 @@
 "use client";
 
 import * as React from "react";
-import dynamic from "next/dynamic";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
-
-const NextThemesProviderClient = dynamic(
-  () => Promise.resolve(NextThemesProvider),
-  { ssr: false }
-);
 
 export function ThemeProvider({
   children,
   ...props
 }: React.ComponentProps<typeof NextThemesProvider>) {
-  return (
-    <NextThemesProviderClient {...props}>
-      {children}
-    </NextThemesProviderClient>
-  );
+  const [mounted, setMounted] = React.useState(false);
+
+  React.useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) {
+    return <>{children}</>;
+  }
+
+  return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
 }
